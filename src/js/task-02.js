c164dd844e2449d84634dba111319c485eca193d
Alena-007 <alena.tv@ukr.net>
2022-07-09 18:03:53 +0300

    add solution task-02

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,14 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const itemEl = document.createElement('li');
+itemEl.class = 'item';
+
+const createLi = array => array.map(el => `<li>${el}</li>`).join('');
+// console.log(createLi(ingredients));
+
+const list = createLi(ingredients);
+// console.log(list);
+
+const listEl = document.querySelector('ul');
+
+listEl.insertAdjacentHTML('beforeend', list);
