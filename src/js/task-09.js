667d3d4d54629e590520bc7943bddd285927a05d
Alena-007 <alena.tv@ukr.net>
2022-07-10 23:26:48 +0300

    add solution task-09

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,18 @@
+const btnEl = document.querySelector('.change-color');
+const spanEl = document.querySelector('.color');
+const bodyEl = document.querySelector('body');
+
+// console.dir(bodyEl);
+
+btnEl.addEventListener('click', changeColor);
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    return `#${Math.floor(Math.random() * 16777215)
+        .toString(16)
+        .padStart(6, 0)}`;
+}
+
+function changeColor() {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+    spanEl.textContent = getRandomHexColor();
 }
