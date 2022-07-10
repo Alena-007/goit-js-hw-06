7dfc61db79063bcf5f986eb32ece6be63743cf57
Alena-007 <alena.tv@ukr.net>
2022-07-10 22:30:50 +0300

    add solution task-08

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,23 @@
+const formEl = document.querySelector('.login-form');
 
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const formElements = event.currentTarget.elements;
+    const email = formElements.email.value;
+    const password = formElements.password.value;
+
+    if (formElements.email.value === '' || formElements.password.value === '') {
+        return alert('All fields must be filled');
+    }
+    const formData = {
+        email,
+        password,
+    };
+
+    formEl.reset();
+
+    console.dir(formData);
+}
